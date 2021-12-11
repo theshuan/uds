ba8c8add1f40232058c0a0670b708c754f90d980
WDS <1039815120@qq.com>
2021-12-12 01:04:09 +0800

    Revert "add uds_init function"
    This reverts commit d0600b5a583759cb0247409f51fbc1ce55229921.

--- CODE/CAN1.c
+++ CODE/CAN1.c
@@ -221,6 +221,7 @@ int main()
 
   
 }
+<<<<<<< HEAD
 
 
 int uds_init()
@@ -238,6 +239,8 @@ int uds()
 =======
 >>>>>>> parent of 0c74f63 (add main function)
 >>>>>>> 3f445fd22a767329728d4daee6c3468c13752371
+=======
+>>>>>>> parent of d0600b5 (add uds_init function)
 /* END CAN1. */
 
 /*
